b1416c942778fafead92c1d082132bd22b50168d
Fallou Thiam <fthiam23344@gmail.com>
2025-12-26 17:06:14 +0000

    Ajoute des deux services (transaction) pour gerer la vente et aussi le changement de prix

---
+++ app/Services/AjouterVenteService.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Varietee;
+use App\Models\Vente;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class AjouterVenteService
+{
+    public function executer($varietee_id, $quantite_vendue)
+    {
+        return DB::transaction(function () use ($varietee_id, $quantite_vendue) {
+
+            // chercher la variete et son prix actuelle via la relation prix_actuelle
+            $varietee = Varietee::with("prix_actuelle")->findOrFail($varietee_id);
+
+            // calculer determiner le prix_unitaire et le prix_totale
+            $prix_unitaire = $varietee->prix_actuelle->prix;
+            $prix_totale = $prix_unitaire * $quantite_vendue;
+
+            // creation de la vente
+            return Vente::create(
+                [
+                    "date_vente" => Carbon::now(),
+                    "quantite_vendu" => $quantite_vendue,
+                    "prix_unitaire" => $prix_unitaire,
+                    "montant_totale" => $prix_totale,
+                    "varietee_id" => $varietee_id
+                ]
+            );
+        });
+    }
+}
---
+++ app/Services/ChangerPrixService.php
@@ -0,0 +1,30 @@
+<?php
+use App\Models\PrixVarietee;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class ChangerPrixService
+{
+    public function executer($varietee_id, $nouveau_montant)
+    {
+        // On cherche le ligne PrixVarietee et ajouter une date_fin
+        return DB::transaction(function () use ($varietee_id, $nouveau_montant) {
+            PrixVarietee::query()->where("varietee_id", $varietee_id)
+                ->whereNull('date_fin')
+                ->lockForUpdate() // evite les multiples update en meme temps
+                ->update([
+                    "date_fin" => Carbon::today(),
+                ]);
+
+            // On creer une nouvelle ligne PrixVarietee
+            $nouveau_prix = PrixVarietee::create([
+                "date_debut" => Carbon::today(),
+                "date_fin" => null,
+                "prix" => $nouveau_montant,
+                "varietee_id" => $varietee_id,
+            ]);
+
+            return $nouveau_prix;
+        });
+    }
+}
