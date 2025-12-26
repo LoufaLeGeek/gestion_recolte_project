6a19e1bfaeba235ea28047eb2a19ca9783f30686
Fallou Thiam <fthiam23344@gmail.com>
2025-12-26 10:32:55 +0000

    Ajout des migrations et de tous modeles et relations entre les modeles

---
+++ app/Models/Varietee.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Varietee extends Model
+{
+    protected $fillable = [
+        "nom_varietee",
+        "caracteristique_varietee",
+        "produit_id",
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function produit()
+    {
+        return $this->belongsTo(Produit::class);
+    }
+
+    public function recoltes()
+    {
+        return $this->hasMany(Recolte::class);
+    }
+
+    public function prix_varietees()
+    {
+        return $this->hasMany(PrixVarietee::class);
+    }
+
+    public function prix_actuelle()
+    {
+        return $this->hasOne(PrixVarietee::class)->whereNull('date_fin');
+    }
+
+    public function stock()
+    {
+        return $this->hasOne(Stock::class);
+    }
+
+    public function ventes()
+    {
+        return $this->hasMany(Vente::class);
+    }
+
+    public function pertes()
+    {
+        return $this->hasMany(Perte::class);
+    }
+}
