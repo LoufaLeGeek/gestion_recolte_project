1c43620c48559e54a75850d332ae42570c355570
Fallou Thiam <fthiam23344@gmail.com>
2025-12-28 14:21:52 +0000

    Revert "Ajout des couleurs de travaille"
    This reverts commit 2ffa64ed960fdd8a0ba3da85f0d3bd463ac65df0.

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,6 +1,6 @@
 @props(['href' => '#', 'content' => '', 'class_icon' => ''])
 <a href="{{ $href }}">
-    <li class="hover:bg-violet hover:text-violet-content">
+    <li>
         <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="{{ $content }}">
             <i class="{{ $class_icon }}"></i>
             <span class="is-drawer-close:hidden">{{ $content }}</span>
