f691ba98824d609acab951e416fa7bd8eac5a737
Fallou Thiam <fthiam23344@gmail.com>
2026-01-01 17:23:02 +0000

    Modification des hover

--- resources/views/components/statistics-kpi-card.blade.php
+++ resources/views/components/statistics-kpi-card.blade.php
@@ -4,7 +4,7 @@
     'text_content' => '',
 ])
 
-<div class="bg-base-100 flex-1 h-full flex flex-col justify-center items-center shadow-xs">
+<div class="bg-base-100 flex-1 h-full flex flex-col justify-center items-center shadow-xs hover:scale-[1.05] hover:bg-base-300/90  duration-200">
     <h1 class="{{ $text_color }} text-center">{{ $value }} {{ $slot }}</h1>
     <span class="font-semibold text-[12px] ">{{ $text_content }}</span>
 </div>
